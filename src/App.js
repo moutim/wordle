3c291145b4d7cb1a07936c18196955df28892802
Vitor <moutimg@gmail.com>
2022-04-16 16:07:53 -0300

    feat: create Wordle page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Switch } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Wordle from './pages/Wordle';
 
 function App() {
   return (
