b6420f132bb18d4b8907e457e473c5f24a1a80fb
Vitor <moutimg@gmail.com>
2022-04-16 20:00:50 -0300

    style: apply borders in letterBox

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './TableRow.css';
 
 export default function TableRow({ guess }) {
   console.log(guess);
   return (
-    <div>
-      <div> <span> { guess[0] } </span> </div>
-      <div> <span> { guess[1] } </span> </div>
-      <div> <span> { guess[2] } </span> </div>
-      <div> <span> { guess[3] } </span> </div>
-      <div> <span> { guess[4] } </span> </div>
+    <div className="containerLetters">
+      <div className="boxLetter"> <span> { guess[0] } </span> </div>
+      <div className="boxLetter"> <span> { guess[1] } </span> </div>
+      <div className="boxLetter"> <span> { guess[2] } </span> </div>
+      <div className="boxLetter"> <span> { guess[3] } </span> </div>
+      <div className="boxLetter"> <span> { guess[4] } </span> </div>
     </div>
   )
 }
