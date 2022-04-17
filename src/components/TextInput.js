fc543622c09862c56006e60ef2f4799d10e62605
Vitor <moutimg@gmail.com>
2022-04-17 14:50:59 -0300

    refactor: change variable name

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -5,7 +5,7 @@ export default function TextInput() {
   const { 
     chances: { attemptNumber, attempts, setAttempts }
   } = useContext(WordleContext);
-  const currentAttempt = attempts[0][`attempt${attemptNumber}`];
+  const currentGuess = attempts[0][`attempt${attemptNumber}`];
 
   const handleValidateInput = ({ target: { value }}) => {
     if (value.length === 5) {
@@ -24,7 +24,7 @@ export default function TextInput() {
         <input 
           type="text" 
           onChange={ handleValidateInput }
-          value={ currentAttempt }
+          value={ currentGuess }
         />
       </label>
     </div>
