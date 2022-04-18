b20c9f0a8bdad3c8317b3490ec891d75283d967a
Vitor <moutimg@gmail.com>
2022-04-18 14:22:15 -0300

    feat: create and render header component on screen

--- src/pages/Wordle.js
+++ src/pages/Wordle.js
@@ -5,11 +5,13 @@ import TextInput from '../components/TextInput';
 import WordleContext from '../context/WordleContext';
 import PlayerWon from '../components/PlayerWon';
 import PlayerLost from '../components/PlayerLost';
+import Header from '../components/Header';
 
 export default function Wordle() {
   const { results: { playerWon, playerLost } } = useContext(WordleContext);
   return (
     <>
+      <Header />
       { playerWon && <PlayerWon /> }
       { playerLost && <PlayerLost /> }
       <Table />
