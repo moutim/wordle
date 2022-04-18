2c065509b449abc8d80d3131a168bb1ceb1d7201
Vitor <moutimg@gmail.com>
2022-04-18 15:37:27 -0300

    feat: create data for use in virtual keyboard

---
+++ src/data/keyboardLetters.js
@@ -0,0 +1,5 @@
+const row1 = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'];
+const row2 = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'];
+const row3 = ['Z', 'X', 'C', 'V', 'B', 'N', 'M'];
+
+export { row1, row2, row3 };
