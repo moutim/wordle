3e9a65ff0740c4c507b3da9a3008f9a33d02791e
Vitor <moutimg@gmail.com>
2022-04-19 21:55:12 -0300

    fix: fix some bugs when player won

--- src/components/PlayerWon.js
+++ src/components/PlayerWon.js
@@ -4,11 +4,19 @@ import './PlayerFeedback.css';
 
 export default function PlayerWon() {
   const { word, chances: { attemptNumber } } = useContext(WordleContext);
+
+  const feedbackMessages = () => {
+    if (attemptNumber < 3) return 'Excelente!';
+    if (attemptNumber > 3 && attemptNumber < 5) return 'Muito bom!';
+    return 'Foi por pouco!'
+  }
+
   return (
     <div className="playerWon">
       <h2>Você acertou!</h2>
       <p>{`A palavra da vez era ${word}`}</p>
       <p>{`Você usou ${attemptNumber - 1} de 6 tentativas`}</p>
+      <p>{ feedbackMessages() }</p>
     </div>
   )
 }
