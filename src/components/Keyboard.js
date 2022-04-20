90f600de290a7546e54944c8b4495395e9a617e4
Vitor <moutimg@gmail.com>
2022-04-19 21:56:27 -0300

    feat: create the functions and the virtual keyboard component

---
+++ src/components/Keyboard.js
@@ -0,0 +1,91 @@
+import React, { useContext } from 'react';
+import WordleContext from '../context/WordleContext';
+import { row1, row2, row3 } from '../data/keyboardLetters';
+import wordlist from '../data/wordlist';
+
+
+export default function Keyboard() {
+  const { 
+    chances: { attemptNumber, attempts, setAttempts, setAttemptNumber },
+    verifications: { verifyAttempt, setVerifyAttempt },
+  } = useContext(WordleContext);
+
+  const handleSendWord = () => {
+    const doesWordExist = wordlist.some(
+      (word) => word.toLowerCase() === attempts[0][`attempt${attemptNumber}`].toLowerCase()
+    );
+    if (doesWordExist && attemptNumber < 6) {
+      setAttemptNumber(attemptNumber + 1);
+      setVerifyAttempt({ ...verifyAttempt, [`attempt${attemptNumber}`]: true });
+    } else setVerifyAttempt({ ...verifyAttempt, [`attempt${attemptNumber}`]: true });
+  }
+
+  const handleKeyPress = ({ key }) => {
+    const currGuess = attempts[0][`attempt${attemptNumber}`];
+    const letters = [...row1, ...row2, ...row3];
+    const isValidKey = letters.some(
+      (letter) => letter.toLowerCase() === key.toLowerCase()
+    );
+    if (key === 'Backspace') {
+      setAttempts([{ 
+        ...attempts[0],
+        [`attempt${attemptNumber}`]: currGuess.substr(0, currGuess.length - 1)
+      }]);
+      return
+    }
+    if (key === 'Enter' && currGuess.length === 5) return handleSendWord();
+    if (isValidKey && currGuess.length < 5) {
+      setAttempts([{ 
+        ...attempts[0],
+        [`attempt${attemptNumber}`]: `${currGuess}${key}`
+      }]);
+    }
+    else console.log('ll');
+  }
+
+  return (
+    <div>
+      <div className="keyboardRow1">
+        {
+          row1.map((letter) => (
+            <button 
+              value={ letter }
+              type="button"
+              // onClick={ handleKeyboard }
+              autoFocus
+              onKeyDown={ handleKeyPress }
+            >
+              { letter }
+            </button>
+          ))
+        }
+      </div>
+      <div className="keyboardRow2">
+        {
+          row2.map((letter) => (
+            <button 
+              value={ letter }
+              type="button"
+              // onClick={ handleKeyboard }
+            >
+              { letter }
+            </button>
+          ))
+        }
+      </div>
+      <div className="keyboardRow3">
+        {
+          row3.map((letter) => (
+            <button 
+              value={ letter }
+              type="button"
+              // onClick={ handleKeyboard }
+            >
+              { letter }
+            </button>
+          ))
+        }
+      </div>
+    </div>
+  )
+}
