f4aa8e8ef5d855148d50178ebbd0341a9dd27d91
unknown <moutimg@gmail.com>
2022-04-23 18:47:52 -0300

    feat: render HowToPlay component and put a icon to show him

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React, { useContext } from 'react';
 import DarkModeToggle from "react-dark-mode-toggle";
 import WordleContext from '../context/WordleContext';
+import questionMark from '../images/questionMark.png';
 
 export default function Header() {
   const { lighting: { darkMode, setDarkMode } } = useContext(WordleContext);
   return (
     <header>
+      <img src={ questionMark } alt="Ilustração de uma lâmpada com um ponto de interrogação dentro." />
       <h1>WORD<span>AY</span></h1>
       <DarkModeToggle
         onChange={ () => setDarkMode(!darkMode) }
