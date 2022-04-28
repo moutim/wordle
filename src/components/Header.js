26f05807b5812140b663b3a10f8358507c1eeb61
unknown <moutimg@gmail.com>
2022-04-28 12:28:01 -0300

    feat: change font style

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,21 @@ import React, { useContext } from 'react';
 import DarkModeToggle from "react-dark-mode-toggle";
 import WordleContext from '../context/WordleContext';
 import questionMark from '../images/questionMark.png';
+import './Header.css';
 
 export default function Header() {
   const { lighting: { darkMode, setDarkMode } } = useContext(WordleContext);
   return (
     <header>
-      <img src={ questionMark } alt="Ilustração de uma lâmpada com um ponto de interrogação dentro." />
+      <img 
+        src={ questionMark } 
+        alt="Ilustração de uma lâmpada com um ponto de interrogação dentro." 
+      />
       <h1>WORD<span>AY</span></h1>
       <DarkModeToggle
         onChange={ () => setDarkMode(!darkMode) }
         checked={ darkMode }
-        size={60}
+        size={ 50 }
         speed={ 1.0 }
       />
     </header>
